f2f806163b2bbdb33e06b3f336bbf51f3eda03d4
Shiva Kumar <shiva2002kumar@gmail.com>
2023-03-25 13:06:11 +0530

    Temprature pressure power value added

--- app/src/main/java/com/example/rghover/MainActivity.java
+++ app/src/main/java/com/example/rghover/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,10 +23,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
 public class MainActivity extends AppCompatActivity {
     Button room1, room2;
     ToggleButton buzzer;
 
+
+    public static class PhysicalData {
+        public Long Temperature;
+        public Long Pressure;
+        public Long Power;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +60,32 @@ public class MainActivity extends AppCompatActivity {
 
         room1 = findViewById(R.id.button1);
         room2 = findViewById(R.id.button2);
-//        room4 = findViewById(R.id.button4);
         buzzer = findViewById(R.id.toggleButton6);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference temp = database.getReference("PhysicalData");
+        temp.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                PhysicalData data = dataSnapshot.getValue(PhysicalData.class);
+                System.out.println("temp = " + data.Temperature + " " + data.Power + " " + data.Pressure);
+                TextView T1, T2, T3;
+                T1 = findViewById(R.id.textView5);
+                T2 = findViewById(R.id.textView7);
+                T3 = findViewById(R.id.textView8);
+                T1.setText(data.Temperature.toString());
+                T2.setText(data.Pressure.toString());
+                T3.setText(data.Power.toString());
+                Log.d(TAG, "ret is: ");
+            }
 
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
         room1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (buzzer.isChecked()) {
-                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+//                    FirebaseDatabase database = FirebaseDatabase.getInstance();
                     DatabaseReference myRef = database.getReference("Pir");
                     myRef.addValueEventListener(new ValueEventListener() {
                         @Override
@@ -104,180 +135,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-//        room4.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                FirebaseDatabase database = FirebaseDatabase.getInstance();
-//                DatabaseReference myRef = database.getReference("Pir");
-//                myRef.addValueEventListener(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(DataSnapshot dataSnapshot) {
-//                        // This method is called once with the initial value and again
-//                        // whenever data at this location is updated.
-//                        String Pir = dataSnapshot.getValue().toString();
-//                        System.out.println("temp = " + Pir);
-//                        Log.d(TAG, "ret is: " + Pir);
-//                        if (Pir == "1") {
-//                            NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "BuzzerNoti")
-//                                    .setSmallIcon(R.drawable.hoverrr)
-//                                    .setContentTitle("Movement Detected")
-//                                    .setContentText("Something is moving")
-//                                    .setAutoCancel(true)
-//                                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-//
-//                            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
-//
-//                            notificationManager.notify(1, builder.build());
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onCancelled(DatabaseError error) {
-//                        // Failed to read value
-//                        Log.w(TAG, "Failed to read value.", error.toException());
-//                    }
-//                });
-//            }
-//        });
-//        button1.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
-//                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-//                    Control = '1' + Control.substring(1);
-//                    System.out.println(Control);
-//                }
-//                else if (motionEvent.getAction() == MotionEvent.ACTION_UP){
-//                    Control = '0' + Control.substring(1);
-//                    System.out.println(Control);
-//                }
-//                return false;
-//            }
-//        });
-
-
-//        button1 = findViewById(R.id.toggleButton);
-//        button2 = findViewById(R.id.toggleButton2);
-//        button3 = findViewById(R.id.toggleButton3);
-//        button4 = findViewById(R.id.toggleButton4);
-//        button1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (button1.isChecked()) {
-//                    Control = Control.substring(0, 0) + '1' + Control.substring(1);
-//                    System.out.println(Control);
-//                    savedata();
-//                } else {
-//                    Control = Control.substring(0, 0) + '0' + Control.substring(1);
-//                    System.out.println(Control);
-//                    savedata();
-//                }
-//            }
-//        });
-//        button2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (button2.isChecked()) {
-//                    Control = Control.substring(0, 1) + '1' + Control.substring(2);
-//                    System.out.println(Control);
-//                    savedata();
-//                } else {
-//                    Control = Control.substring(0, 1) + '0' + Control.substring(2);
-//                    System.out.println(Control);
-//                    savedata();
-//                }
-//            }
-//        });
-//        button3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (button3.isChecked()) {
-//                    Control = Control.substring(0, 2) + '1' + Control.substring(3);
-//                    System.out.println(Control);
-//                    savedata();
-//                } else {
-//                    Control = Control.substring(0, 2) + '0' + Control.substring(3);
-//                    System.out.println(Control);
-//                    savedata();
-//                }
-//            }
-//        });
-//        button4.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (button4.isChecked()) {
-//                    Control = Control.substring(0, 3) + '1' + Control.substring(4);
-//                    System.out.println(Control);
-//                    savedata();
-//                } else {
-//                    Control = Control.substring(0, 3) + '0' + Control.substring(4);
-//                    System.out.println(Control);
-//                    savedata();
-//                }
-//            }
-//        });
-
-//        switch1 = findViewById(R.id.switch2);
-//        switch2 = findViewById(R.id.switch3);
-//        switch3 = findViewById(R.id.switch4);
-//        switch4 = findViewById(R.id.switch5);
-//
-//        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-//                if (b) {
-//                    Control = Control.substring(0, 0) + '1' + Control.substring(1);
-//                    System.out.println(Control);
-//                    savedata();
-//                } else {
-//                    Control = Control.substring(0, 0) + '0' + Control.substring(1);
-//                    System.out.println(Control);
-//                    savedata();
-//                }
-//            }
-//        });
-//        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-//                if (b) {
-//                    Control = Control.substring(0, 1) + '1' + Control.substring(2);
-//                    System.out.println(Control);
-//                    savedata();
-//                } else {
-//                    Control = Control.substring(0, 1) + '0' + Control.substring(2);
-//                    System.out.println(Control);
-//                    savedata();
-//                }
-//            }
-//        });
-//        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-//                if (b) {
-//                    Control = Control.substring(0, 2) + '1' + Control.substring(3);
-//                    System.out.println(Control);
-//                    savedata();
-//                } else {
-//                    Control = Control.substring(0, 2) + '0' + Control.substring(3);
-//                    System.out.println(Control);
-//                    savedata();
-//                }
-//            }
-//        });
-//        switch4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-//                if (b) {
-//                    Control = Control.substring(0, 3) + '1' + Control.substring(4);
-//                    System.out.println(Control);
-//                    savedata();
-//                } else {
-//                    Control = Control.substring(0, 3) + '0' + Control.substring(4);
-//                    System.out.println(Control);
-//                    savedata();
-//                }
-//            }
-//        });
-
     }
 }
--- app/src/main/java/com/example/rghover/Room.java
+++ app/src/main/java/com/example/rghover/Room.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.google.firebase.database.DataSnapshot;
@@ -29,6 +30,31 @@ public class Room extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
+        DatabaseReference temp = database.getReference("PhysicalData");
+        temp.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                MainActivity.PhysicalData data = dataSnapshot.getValue(MainActivity.PhysicalData.class);
+                System.out.println("temp = " + data.Temperature + " " + data.Power + " " + data.Pressure);
+                TextView T1, T2, T3;
+                T1 = findViewById(R.id.textView5);
+                T2 = findViewById(R.id.textView7);
+                T3 = findViewById(R.id.textView8);
+                T1.setText(data.Temperature.toString());
+                T2.setText(data.Pressure.toString());
+                T3.setText(data.Power.toString());
+                Log.d(TAG, "ret is: ");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
         int room = getIntent().getIntExtra("roomnum", 0);
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
